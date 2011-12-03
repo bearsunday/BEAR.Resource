43fc8c10116c12596e89277a05a83f7d846c788a
koriym <akihito.koriyama@gmail.com>
2011-12-03 10:08:20 +0900

    Guzzle autoloader.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -27,3 +27,15 @@ require_once __DIR__ . '/Mock/ResourceObject/RestBucks/Menu.php';
 require_once __DIR__ . '/Mock/Page/News.php';
 require_once __DIR__ . '/Mock/Interceptor/Log.php';
 require_once __DIR__ . '/Mock/MockModule.php';
+
+$base = (dirname(__DIR__));
+require_once $base . '/vendors/Guzzle/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+$classLoader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
+$classLoader->registerNamespaces(array(
+            'Guzzle\Tests' => __DIR__,
+            'Guzzle' => $base . '/vendors/Guzzle/src',
+            'Doctrine' => $base . '/vendors/Guzzle/vendor/Doctrine/lib',
+            'Monolog' => $base . '/vendors/Guzzle/vendor/Monolog/src'
+));
+$classLoader->registerPrefix('Zend_', $base . '/vendors/Guzzle/vendor');
+$classLoader->register();
