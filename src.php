c1b072184ee3edecc0253ea79bad76a80fa7354d
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-02-17 04:14:44 +0900

    PSR0-layout.

--- src.php
+++ src.php
@@ -3,52 +3,52 @@
 namespace PHP\Resource;
 
 // interface
-require_once __DIR__ . '/src/Provider.php';
-require_once __DIR__ . '/src/Resource.php';
-require_once __DIR__ . '/src/Object.php';
-require_once __DIR__ . '/src/Invokable.php';
-require_once __DIR__ . '/src/Uri.php';
-require_once __DIR__ . '/src/Linkable.php';
-require_once __DIR__ . '/src/LinkType.php';
+require_once __DIR__ . '/src/BEAR/Resource/Provider.php';
+require_once __DIR__ . '/src/BEAR/Resource/Resource.php';
+require_once __DIR__ . '/src/BEAR/Resource/Object.php';
+require_once __DIR__ . '/src/BEAR/Resource/Invokable.php';
+require_once __DIR__ . '/src/BEAR/Resource/Uri.php';
+require_once __DIR__ . '/src/BEAR/Resource/Linkable.php';
+require_once __DIR__ . '/src/BEAR/Resource/LinkType.php';
 
 // trait
-require_once __DIR__ . '/src/ArrayAccess.php';
+require_once __DIR__ . '/src/BEAR/Resource/ArrayAccess.php';
 
 // abstract class
-require_once __DIR__ . '/src/AbstractObject.php';
+require_once __DIR__ . '/src/BEAR/Resource/AbstractObject.php';
 
 // cocrete class
-require_once __DIR__ . '/src/Object.php';
-require_once __DIR__ . '/src/Request.php';
-require_once __DIR__ . '/src/Client.php';
-require_once __DIR__ . '/src/ResourceFactory.php';
-require_once __DIR__ . '/src/Factory.php';
-require_once __DIR__ . '/src/Invoker.php';
-require_once __DIR__ . '/src/Linker.php';
-require_once __DIR__ . '/src/SchemeCollection.php';
-require_once __DIR__ . '/src/Adapter/App.php';
-require_once __DIR__ . '/src/Adapter/App/Link.php';
-require_once __DIR__ . '/src/Adapter/Page.php';
-require_once __DIR__ . '/src/Adapter/Http.php';
-require_once __DIR__ . '/src/Adapter/Http/HttpClient.php';
-require_once __DIR__ . '/src/Adapter/Http/Guzzle.php';
+require_once __DIR__ . '/src/BEAR/Resource/Object.php';
+require_once __DIR__ . '/src/BEAR/Resource/Request.php';
+require_once __DIR__ . '/src/BEAR/Resource/Client.php';
+require_once __DIR__ . '/src/BEAR/Resource/ResourceFactory.php';
+require_once __DIR__ . '/src/BEAR/Resource/Factory.php';
+require_once __DIR__ . '/src/BEAR/Resource/Invoker.php';
+require_once __DIR__ . '/src/BEAR/Resource/Linker.php';
+require_once __DIR__ . '/src/BEAR/Resource/SchemeCollection.php';
+require_once __DIR__ . '/src/BEAR/Resource/Adapter/App.php';
+require_once __DIR__ . '/src/BEAR/Resource/Adapter/App/Link.php';
+require_once __DIR__ . '/src/BEAR/Resource/Adapter/Page.php';
+require_once __DIR__ . '/src/BEAR/Resource/Adapter/Http.php';
+require_once __DIR__ . '/src/BEAR/Resource/Adapter/Http/HttpClient.php';
+require_once __DIR__ . '/src/BEAR/Resource/Adapter/Http/Guzzle.php';
 
-require_once __DIR__ . '/src/Annotation/Signal.php';
-require_once __DIR__ . '/src/Annotation/ArgSignal.php';
-require_once __DIR__ . '/src/Annotation/Provides.php';
+require_once __DIR__ . '/src/BEAR/Resource/Annotation/Signal.php';
+require_once __DIR__ . '/src/BEAR/Resource/Annotation/ArgSignal.php';
+require_once __DIR__ . '/src/BEAR/Resource/Annotation/Provides.php';
 
 // exception
-require_once __DIR__ . '/src/Exception.php';
-require_once __DIR__ . '/src/Exception/Factory.php';
-require_once __DIR__ . '/src/Exception/InvalidParameter.php';
-require_once __DIR__ . '/src/Exception/InvalidMethod.php';
-require_once __DIR__ . '/src/Exception/InvalidHost.php';
-require_once __DIR__ . '/src/Exception/InvalidScheme.php';
-require_once __DIR__ . '/src/Exception/InvalidRequest.php';
-require_once __DIR__ . '/src/Exception/InvalidLink.php';
-require_once __DIR__ . '/src/Exception/InvalidUri.php';
-require_once __DIR__ . '/src/Exception/BadRequest.php';
-require_once __DIR__ . '/src/Exception/MethodNotAllowed.php';
+require_once __DIR__ . '/src/BEAR/Resource/Exception.php';
+require_once __DIR__ . '/src/BEAR/Resource/Exception/Factory.php';
+require_once __DIR__ . '/src/BEAR/Resource/Exception/InvalidParameter.php';
+require_once __DIR__ . '/src/BEAR/Resource/Exception/InvalidMethod.php';
+require_once __DIR__ . '/src/BEAR/Resource/Exception/InvalidHost.php';
+require_once __DIR__ . '/src/BEAR/Resource/Exception/InvalidScheme.php';
+require_once __DIR__ . '/src/BEAR/Resource/Exception/InvalidRequest.php';
+require_once __DIR__ . '/src/BEAR/Resource/Exception/InvalidLink.php';
+require_once __DIR__ . '/src/BEAR/Resource/Exception/InvalidUri.php';
+require_once __DIR__ . '/src/BEAR/Resource/Exception/BadRequest.php';
+require_once __DIR__ . '/src/BEAR/Resource/Exception/MethodNotAllowed.php';
 
 // constants
-require_once __DIR__ . '/src/Code.php';
+require_once __DIR__ . '/src/BEAR/Resource/Code.php';
