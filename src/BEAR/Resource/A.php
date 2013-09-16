600dfdbe4f4b1bbc54eaeae869eb15b6b737482f
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-09-17 06:37:50 +0900

    add A as anchor

---
+++ src/BEAR/Resource/A.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This file is part of the BEAR.Resource package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Resource;
+
+use BEAR\Resource\Exception;
+use Guzzle\Parser\UriTemplate\UriTemplateInterface;
+use Ray\Di\Di\Inject;
+use Ray\Di\Di\Scope;
+
+/**
+ * Anchor
+ */
+class A implements HrefInterface
+{
+    /**
+     * @param UriTemplateInterface $uriTemplate
+     *
+     * @Inject
+     */
+    public function __construct(UriTemplateInterface $uriTemplate)
+    {
+        $this->uriTemplate = $uriTemplate;
+    }
+
+    /**
+     * Return hyper reference URI
+     *
+     * @param string         $rel
+     * @param ResourceObject $ro
+     *
+     * @return mixed|string
+     * @throws Exception\Link
+     */
+    public function href($rel, ResourceObject $ro)
+    {
+        if (!isset($ro->links[$rel])) {
+            throw new Exception\Link(get_class($ro) . ':' . $rel);
+        }
+        $link = $ro->links[$rel];
+        $isTemplated = (isset($link[Link::TEMPLATED]) && $link[Link::TEMPLATED] === true);
+        $uri = $isTemplated ? $this->uriTemplate->expand($link[Link::HREF], $ro->body) : $link[Link::HREF];
+
+        return $uri;
+    }
+}
