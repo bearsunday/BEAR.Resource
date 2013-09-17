32c980d84c7747ef26d4767e54021720902e1f23
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-09-18 00:29:13 +0900

    meta

---
+++ tests/BEAR/Resource/ATest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace BEAR\Resource;
+
+use Guzzle\Parser\UriTemplate\UriTemplate;
+use Sandbox\Resource\App\User;
+
+class ATest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->a = new A(new UriTemplate);
+    }
+
+    public function testNew()
+    {
+        $this->assertInstanceOf('\BEAR\Resource\A', $this->a);
+    }
+
+    /**
+     * @expectedException \BEAR\Resource\Exception\Link
+     */
+    public function testHrefInvalidRel()
+    {
+        $resource = new Mock\Entry;
+        $this->a->href('not_found', $resource);
+    }
+
+    public function testHrefWithTemplateUri()
+    {
+        $resource = new Mock\User;
+        $uri = $this->a->href('friend', $resource);
+        $this->assertSame('app://self/friend/?id=1', $uri);
+    }
+
+    public function testHrefWithoutTemplateUri()
+    {
+        $resource = new Mock\User;
+        $uri = $this->a->href('profile', $resource);
+        $this->assertSame('app://self/profile', $uri);
+    }
+}
