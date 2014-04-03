8f6054cc0c0579e539108abf6489e714529210e3
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-04-04 02:17:51 +0900

    add @codeCoverageIgnoreStart
    for PHP5.4 / 5.5 json difference

--- src/Renderer/JsonRenderer.php
+++ src/Renderer/JsonRenderer.php
@@ -19,8 +19,10 @@ class JsonRenderer implements RenderInterface
         $ro->view = @json_encode($ro);
         $e = json_last_error();
         if ($e) {
+            // @codeCoverageIgnoreStart
             error_log('json_encode error: ' . json_last_error_msg() . ' in ' . __METHOD__);
             return '';
+            // @codeCoverageIgnoreEnd
         }
 
         return $ro->view;
