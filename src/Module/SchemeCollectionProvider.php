5d32bb26dd4cc918790d9e763d08a01ff24c8280
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-05-22 13:42:46 +0900

    $appName validation

--- src/Module/SchemeCollectionProvider.php
+++ src/Module/SchemeCollectionProvider.php
@@ -43,7 +43,7 @@ class SchemeCollectionProvider implements Provide
      */
     public function setAppName($appName, $resourceDir)
     {
-        if (is_null($appName)) {
+        if (! is_string($appName)) {
             throw new AppName($appName);
         }
         $this->appName = $appName;
