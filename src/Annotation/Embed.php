b191b1d1fe68d3a4ef20cf0eb79a2d37e9c765e4
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-11-12 10:18:26 +0900

    refactor annotations
    remove @ParamSignal @Provides, @Signal and interface

--- src/Annotation/Embed.php
+++ src/Annotation/Embed.php
@@ -10,7 +10,7 @@ namespace BEAR\Resource\Annotation;
  * @Annotation
  * @Target("METHOD")
  */
-final class Embed implements AnnotationInterface
+final class Embed
 {
     /**
      * Relation
--- src/Annotation/Link.php
+++ src/Annotation/Link.php
@@ -10,7 +10,7 @@ namespace BEAR\Resource\Annotation;
  * @Annotation
  * @Target("METHOD")
  */
-final class Link implements AnnotationInterface
+final class Link
 {
     const REL = 'rel';
 
