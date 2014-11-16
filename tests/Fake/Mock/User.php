ccb242c35f285687eb84f0c39fc35e7c8b65556e
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-11-17 01:10:15 +0900

    remove Link constants class

--- tests/Fake/Mock/User.php
+++ tests/Fake/Mock/User.php
@@ -3,14 +3,14 @@
 namespace BEAR\Resource\Mock;
 
 use BEAR\Resource;
-use BEAR\Resource\Annotation\Link;
 use BEAR\Resource\ResourceObject;
+use BEAR\Resource\Annotation\Link;
 
 class User extends ResourceObject
 {
     public $links = [
-        'friend' => [Resource\Link::HREF => 'app://self/friend/{?id}', Resource\Link::TEMPLATED => true],
-        'profile' => [Resource\Link::HREF => 'app://self/profile']
+        'friend' => [Link::HREF => 'app://self/friend/{?id}', Link::TEMPLATED => true],
+        'profile' => [Link::HREF => 'app://self/profile']
     ];
 
     public $body = [
