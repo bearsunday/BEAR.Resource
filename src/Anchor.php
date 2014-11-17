5b941896756b53c5466d0c3d05e8bdd8e95cb188
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-11-17 16:55:23 +0900

    Extract Anchor::href

--- src/Anchor.php
+++ src/Anchor.php
@@ -28,16 +28,40 @@ class Anchor implements AnchorInterface
         $classMethod = 'on' . ucfirst($request->method);
         $annotations = $this->reader->getMethodAnnotations(new \ReflectionMethod($request->resourceObject, $classMethod));
         foreach ($annotations as $annotation) {
-            $isValidLinkAnnotation = $annotation instanceof LinkAnnotation && isset($annotation->rel) && $annotation->rel === $rel;
-            if ($isValidLinkAnnotation) {
-                $body = $request->resourceObject->body;
-                $query = is_array($body) ? array_merge($body, $query) : [];
-                $uri = \GuzzleHttp\uri_template($annotation->href, $query);
-
-                return [$annotation->method, $uri];
+            if ($this->isValidLinkAnnotation($annotation, $rel)) {
+                return $this->getMethodUdi($request, $query, $annotation);
             }
         }
 
         throw new LinkException("rel:{$rel} class:" . get_class($request->resourceObject));
     }
+
+    /**
+     * @param LinkAnnotation $annotation
+     * @param string         $rel
+     *
+     * @return bool
+     */
+    private function isValidLinkAnnotation($annotation, $rel)
+    {
+        return $annotation instanceof LinkAnnotation && isset($annotation->rel) && $annotation->rel === $rel;
+
+    }
+
+    /**
+     * @param AbstractRequest $request
+     * @param array           $query
+     * @param LinkAnnotation  $annotation
+     *
+     * @return array
+     */
+    private function getMethodUdi(AbstractRequest $request, array $query, LinkAnnotation $annotation)
+    {
+        $body = $request->resourceObject->body;
+        $query = is_array($body) ? array_merge($body, $query) : [];
+        $uri = \GuzzleHttp\uri_template($annotation->href, $query);
+
+        return [$annotation->method, $uri];
+    }
+
 }
