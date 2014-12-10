1af48e1aa383a8fecdaf5fb2d8b3642184d01090
koriym <akihito.koriyama@gmail.com>
2014-12-10 17:04:21 +0900

    add function comment

--- src-function/functions.php
+++ src-function/functions.php
@@ -1,6 +1,16 @@
 <?php
 
 if (! function_exists('\\uri_template')) {
+    /**
+     * Implementation of URI Template(RFC6570) specification for PHP
+     *
+     * @param string $template
+     * @param array  $variables
+     *
+     * @return string
+     *
+     * @see http://pecl.php.net/package/uri_template
+     */
     function uri_template($template, array $variables)
     {
         static $uriTemplate;
