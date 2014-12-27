a7add04dba43437cc543456a6cd6c5d65450ef0c
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-12-27 20:25:18 +0900

    add ResourceClientFactory::newClient()

--- src/ResourceClientFactory.php
+++ src/ResourceClientFactory.php
@@ -6,24 +6,52 @@
  */
 namespace BEAR\Resource;
 
+use Doctrine\Common\Annotations\AnnotationReader;
+use Ray\Di\AbstractModule;
 use Ray\Di\Injector;
 use Doctrine\Common\Annotations\Reader;
 
 class ResourceClientFactory
 {
     /**
+     * @var Injector
+     */
+    private $injector;
+
+    public function __construct()
+    {
+        $this->injector = new Injector;
+    }
+
+    /**
+     * @param string         $tmpDir
+     * @param string         $namespace
+     * @param AbstractModule $module
+     *
+     * @return Resource
+     */
+    public function newClient($tmpDir, $namespace, AbstractModule $module = null)
+    {
+        $this->injector = $module ? new Injector($module, $tmpDir) : new Injector(null, $tmpDir);
+
+        return $this->newInstance($namespace, new AnnotationReader);
+    }
+
+    /**
      * @param string           $namespace
      * @param Reader           $reader
      * @param SchemeCollection $scheme
+     * @param AbstractModule   $module
      *
      * @return \BEAR\Resource\Resource
+     *
+     * @deprecated use newClient
      */
     public function newInstance($namespace, Reader $reader, SchemeCollection $scheme = null)
     {
-        $injector = new Injector;
         $scheme = $scheme ?: $scheme = (new SchemeCollection)
-            ->scheme('app')->host('self')->toAdapter(new AppAdapter($injector, $namespace, 'Resource\App'))
-            ->scheme('page')->host('self')->toAdapter(new AppAdapter($injector, $namespace, 'Resource\Page'));
+            ->scheme('app')->host('self')->toAdapter(new AppAdapter($this->injector, $namespace, 'Resource\App'))
+            ->scheme('page')->host('self')->toAdapter(new AppAdapter($this->injector, $namespace, 'Resource\Page'));
         $invoker = new Invoker(new NamedParameter);
         $factory = new Factory($scheme);
         $resource = new Resource(
