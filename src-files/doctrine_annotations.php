9c22044628d9261ab914575c489b77aef1f5085d
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-10-15 21:06:29 +0900

    auto loader for annotations

--- src-files/doctrine_annotations.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once dirname(__DIR__) . '/src/Annotation/Embed.php';
-require_once dirname(__DIR__) . '/src/Annotation/Link.php';
-require_once dirname(__DIR__) . '/src/Annotation/AppName.php';
-require_once dirname(__DIR__) . '/src/Annotation/LogicCache.php';
-require_once dirname(__DIR__) . '/src/Annotation/ResourceParam.php';
-require_once dirname(__DIR__) . '/src/Annotation/ImportAppConfig.php';
