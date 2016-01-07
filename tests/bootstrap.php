6c9d37307e73e9d6df80790d567f5ae9588e1005
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-01-07 12:01:57 +0900

    cleanup on shutdown

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,17 +1,19 @@
 <?php
+
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$loader = require dirname(__DIR__) . '/vendor/autoload.php';
+$loader = require dirname(__DIR__).'/vendor/autoload.php';
 /* @var $loader \Composer\Autoload\ClassLoader */
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
-$_ENV['TMP_DIR'] = __DIR__ . '/tmp';
-$_ENV['PACKAGE_DIR'] = dirname(__DIR__);
-$_ENV['TEST_DIR'] = __DIR__;
-$rm = function ($dir) use (&$rm) {
-    foreach (glob($dir . '/*') as $file) {
-        is_dir($file) ? $rm($file) : unlink($file);
+$_ENV['TMP_DIR'] = __DIR__.'/tmp';
+$unlink = function ($path) use (&$unlink) {
+    foreach (glob(rtrim($path, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR . '*') as $file) {
+        is_dir($file) ? $unlink($file) : unlink($file);
         @rmdir($file);
     }
 };
-$rm($_ENV['TMP_DIR']);
+$unlink($_ENV['TMP_DIR']);
+register_shutdown_function(function () use ($unlink) {
+    $unlink($_ENV['TMP_DIR']);
+});
