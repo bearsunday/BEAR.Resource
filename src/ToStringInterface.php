e643e91e101655c322989e0eceddf18b57ed778f
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-10-01 11:15:24 +0900

    add ToStringInterface

---
+++ src/ToStringInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the BEAR.Resource package
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\Resource;
+
+interface ToStringInterface
+{
+    /**
+     * Return string representation
+     *
+     * @return string
+     */
+    public function toString();
+}
