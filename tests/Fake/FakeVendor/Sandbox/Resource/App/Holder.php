c61a90b2fe7fe347715b23233cd66fab9411854c
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-05-01 16:22:14 +0900

    add fail test

---
+++ tests/Fake/FakeVendor/Sandbox/Resource/App/Holder.php
@@ -0,0 +1,18 @@
+<?php
+namespace FakeVendor\Sandbox\Resource\App;
+
+use BEAR\Resource\ResourceInterface;
+use BEAR\Resource\ResourceObject;
+
+class Holder extends ResourceObject
+{
+    public function __construct(ResourceInterface $resource)
+    {
+        $resource->get->uri('app://self/author?id=1')->eager->request();
+    }
+
+    public function onPost()
+    {
+        return true;
+    }
+}
