71c3730ebb94e4a52eb6e64ed8b9af164ba81850
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-05-02 17:40:58 +0900

    set request method to resource object

--- src/Resource.php
+++ src/Resource.php
@@ -122,13 +122,13 @@ final class Resource implements ResourceInterface
         if (is_string($uri)) {
             $uri = new Uri($uri);
         }
-        $method = $this->method;
+        $uri->method = $this->method;
         $resourceObject = $this->newInstance($uri);
         $resourceObject->uri = $uri;
         $this->request = new Request(
             $this->invoker,
             $resourceObject,
-            $method,
+            $uri->method,
             $uri->query,
             [],
             $this->linker
