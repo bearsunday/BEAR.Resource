71b5a0fa76b2adc5e81f39dccf3a67e1e5ffb3b4
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-05-18 07:09:35 +0900

    add OptionsMethodModule

---
+++ src/Module/OptionsMethodModule.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the BEAR.Resource package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\Resource\Module;
+
+use BEAR\Resource\OptionsRenderer;
+use BEAR\Resource\RenderInterface;
+use Ray\Di\AbstractModule;
+
+class OptionsMethodModule extends AbstractModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->bind(RenderInterface::class)->annotatedWith('options')->to(OptionsRenderer::class);
+    }
+}
