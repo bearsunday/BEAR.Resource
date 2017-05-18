7bf5eafe6f6537db0210ba071aa30436d48404f9
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-05-19 03:26:29 +0900

    add null default sample

--- tests/Fake/FakeVendor/Sandbox/Resource/App/DocPhp7.php
+++ tests/Fake/FakeVendor/Sandbox/Resource/App/DocPhp7.php
@@ -2,7 +2,9 @@
 
 namespace FakeVendor\Sandbox\Resource\App;
 
+use BEAR\Resource\Annotation\ResourceParam;
 use BEAR\Resource\ResourceObject;
+use Ray\Di\Di\Assisted;
 
 class DocPhp7 extends ResourceObject
 {
@@ -10,12 +12,14 @@ class DocPhp7 extends ResourceObject
      * @param int    $id          Id
      * @param string $name        Name
      * @param bool   $sw          Swithc
+     * @param string $login_id    Login ID
      * @param array  $arr
      * @param string $defaultNull DefaultNull
      *
-     * @return $this
+     * @ResourceParam(param="login_id", uri="app://self/login#id")
+     * @Assisted({"time"})
      */
-    public function onGet(int $id, string $name, bool $sw, array $arr, $defaultNull = null)
+    public function onGet(int $id, string $name, bool $sw, string $login_id, array $arr, string $time, $defaultNull = null)
     {
         return $this;
     }
