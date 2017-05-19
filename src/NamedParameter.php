dad8034e96b3e5d135f0d0bf3089b40c5b302f76
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-05-19 13:44:23 +0900

    disallow override @ResourceParam @Assisted value with withQuery() method

--- src/NamedParameter.php
+++ src/NamedParameter.php
@@ -6,7 +6,13 @@
  */
 namespace BEAR\Resource;
 
+use BEAR\Resource\Annotation\ResourceParam;
+use BEAR\Resource\Exception\ParameterException;
+use Doctrine\Common\Annotations\Reader;
 use Doctrine\Common\Cache\Cache;
+use Ray\Di\Di\Assisted;
+use Ray\Di\Injector;
+use Ray\Di\InjectorInterface;
 
 final class NamedParameter implements NamedParameterInterface
 {
@@ -16,14 +22,20 @@ final class NamedParameter implements NamedParameterInterface
     private $cache;
 
     /**
-     * @var ParameterHandlerInterface
+     * @var Reader
      */
-    private $handler;
+    private $reader;
 
-    public function __construct(Cache $cache, ParameterHandlerInterface $handler)
+    /**
+     * @var InjectorInterface
+     */
+    private $injector;
+
+    public function __construct(Cache $cache, Reader $reader, InjectorInterface $injector)
     {
         $this->cache = $cache;
-        $this->handler = $handler;
+        $this->reader = $reader;
+        $this->injector = $injector;
     }
 
     /**
@@ -34,7 +46,7 @@ final class NamedParameter implements NamedParameterInterface
         $id = __CLASS__ . get_class($callable[0]) . $callable[1];
         $names = $this->cache->fetch($id);
         if (! $names) {
-            $names = $this->getParameterNames($callable);
+            $names = $this->getNamedParamMetas($callable);
             $this->cache->save($id, $names);
         }
         $parameters = $this->handleParams($query, $names);
@@ -42,36 +54,79 @@ final class NamedParameter implements NamedParameterInterface
         return $parameters;
     }
 
-    private function getParameterNames(array $callable)
+    /**
+     * Return named parameter information
+     *
+     * @param array $callable
+     *
+     * @return array
+     */
+    private function getNamedParamMetas(array $callable)
     {
-        $parameters = (new \ReflectionMethod($callable[0], $callable[1]))->getParameters();
+        $method = new \ReflectionMethod($callable[0], $callable[1]);
+        $parameters = $method->getParameters();
         $names = [];
         foreach ($parameters as $parameter) {
-            $default = $parameter->isDefaultValueAvailable() === true && $parameter->getDefaultValue() !== null ? $parameter->getDefaultValue() : new Param(get_class($callable[0]), $callable[1], $parameter->name);
+            $default = $parameter->isDefaultValueAvailable() === true ? $parameter->getDefaultValue() : new Param(get_class($callable[0]), $callable[1], $parameter->name);
             $names[$parameter->name] = $default;
+        }
+        $annotations = $this->reader->getMethodAnnotations($method);
+        foreach ($annotations as $annotation) {
+            if ($annotation instanceof ResourceParam) {
+                $names[$annotation->param] = $annotation;
+            }
+            if ($annotation instanceof Assisted) {
+                $names[$annotation->param] = $annotation;
+            }
         }
 
         return $names;
     }
 
     /**
-     * @param string[] $query
-     * @param string[] $names
+     * @param string[] $query caller value
+     * @param string[] $names default value ['param-name' => 'param-type|param-value']
      *
      * @return array
      */
     private function handleParams(array $query, array $names)
     {
         $parameters = [];
-        foreach ($names as $name => $defaultValue) {
-            $value = array_key_exists($name, $query) ? $query[$name] : $defaultValue;
-            if ($value instanceof Param) {
-                $parameter = new \ReflectionParameter([$value->class, $value->method], $value->param);
-                $value = $this->handler->handle($parameter, $query);
+        foreach ($names as $name => $param) {
+            // @ResourceParam value
+            if ($param instanceof ResourceParam) {
+                $parameters[] = $this->getResourceParam($param, $query);
+                continue;
             }
-            $parameters[] = $value;
+            // caller value but not @Assisted
+            if (! $param instanceof Assisted && isset($query[$name])) {
+                $parameters[] = $query[$name];
+                continue;
+            }
+            // default value
+            if (is_scalar($param) || $param === null) {
+                $parameters[] = $param;
+                continue;
+            }
+            throw new ParameterException($name);
         }
 
         return $parameters;
+    }
+
+    /**
+     * @param ResourceParam $resourceParam
+     * @param array         $query
+     *
+     * @return mixed
+     */
+    private function getResourceParam(ResourceParam $resourceParam, array $query)
+    {
+        $uri = $resourceParam->templated === true ? uri_template($resourceParam->uri, $query) : $resourceParam->uri;
+        $resource = $this->injector->getInstance(ResourceInterface::class);
+        $resourceResult = $resource->get->uri($uri)->eager->request();
+        $fragment = parse_url($uri, PHP_URL_FRAGMENT);
+
+        return $resourceResult[$fragment];
     }
 }
