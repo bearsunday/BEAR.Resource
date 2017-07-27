0953cbab57e412aaacea19f91864833c6029d357
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-07-28 02:36:49 +0900

    add VoidOptionsMethodModule test

---
+++ tests/VoidOptionsRendererTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the BEAR.Resource package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\Resource;
+
+use BEAR\Resource\Module\ResourceModule;
+use BEAR\Resource\Module\VoidOptionsMethodModule;
+use Ray\Di\Injector;
+
+class VoidOptionsRendererTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \BEAR\Resource\Exception\MethodNotAllowedException
+     */
+    public function testVoidOptionsRenderer()
+    {
+        $injector = new Injector(new VoidOptionsMethodModule(new FakeSchemeModule(new ResourceModule('FakeVendor\Sandbox')), $_ENV['TMP_DIR']));
+        $resource = $injector->getInstance(ResourceInterface::class);
+        /* @var $resource \BEAR\Resource\ResourceInterface */
+        $resource->options->uri('page://self/index')->eager->request();
+    }
+}
