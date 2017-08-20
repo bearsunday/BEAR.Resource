c9e81a35789e0931f09932fb2f8d8cd274d0cc37
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-20 20:09:46 +0900

    add pretty json renderer

---
+++ src/PrettyJsonRenderer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the BEAR.Resource package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\Resource;
+
+final class PrettyJsonRenderer implements RenderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function render(ResourceObject $ro)
+    {
+        if (! array_key_exists('content-type', $ro->headers)) {
+            $ro->headers['content-type'] = 'application/json';
+        }
+        $ro->view = json_encode($ro, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . PHP_EOL;
+        $e = json_last_error();
+        if ($e) {
+            // @codeCoverageIgnoreStart
+            error_log('json_encode error: ' . json_last_error_msg() . ' in ' . __METHOD__);
+
+            return '';
+            // @codeCoverageIgnoreEnd
+        }
+
+        return $ro->view;
+    }
+}
