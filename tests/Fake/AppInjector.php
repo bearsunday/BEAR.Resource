b3cb0555a841e3ecb95c3ff77d44c3f77291bbf1
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-01 12:37:47 +0900

    implement fake AppInjector

--- tests/Fake/AppInjector.php
+++ tests/Fake/AppInjector.php
@@ -6,16 +6,25 @@
  */
 namespace BEAR\Package;
 
+use FakeVendor\Sandbox\Module\AppModule;
+use Ray\Di\Injector;
 use Ray\Di\InjectorInterface;
 use Ray\Di\Name;
 
+/**
+ * Fake AppInjector for unit test
+ */
 class AppInjector implements InjectorInterface
 {
-    public function __construct(string $appName, string $context)
+    public function __construct($appName, $context)
     {
+        unset($appName, $context);
     }
 
     public function getInstance($interface, $name = Name::ANY)
     {
+        $injector = new Injector(new AppModule);
+
+        return $injector->getInstance($interface, $name);
     }
 }
