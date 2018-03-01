9f414b55811b359c3ccc4736e0cc6577718a32da
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-01 10:31:52 +0900

    check type

--- src/Interceptor/JsonSchemaInterceptor.php
+++ src/Interceptor/JsonSchemaInterceptor.php
@@ -48,14 +48,18 @@ final class JsonSchemaInterceptor implements MethodInterceptor
      */
     public function invoke(MethodInvocation $invocation)
     {
-        /* @var $jsonSchema JsonSchema */
         $jsonSchema = $invocation->getMethod()->getAnnotation(JsonSchema::class);
+        if (! $jsonSchema instanceof JsonSchema) {
+            throw new JsonSchemaException($invocation->getMethod()->name);
+        }
         if ($jsonSchema->params) {
             $arguments = $this->getNamedArguments($invocation);
             $this->validateRequest($jsonSchema, $arguments);
         }
-        /* @var $ro ResourceObject */
         $ro = $invocation->proceed();
+        if (! $ro instanceof ResourceObject) {
+            throw new JsonSchemaException($invocation->getMethod()->name);
+        }
         if ($ro->code === 200 || $ro->code == 201) {
             $this->validateResponse($jsonSchema, $ro);
         }
