812cd6ada0bc28668961e3b494fda88bf71337a6
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-01 12:45:27 +0900

    remove redundnt constructor

--- tests/Fake/FakeVendor/Sandbox/Resource/App/Weave/Book.php
+++ tests/Fake/FakeVendor/Sandbox/Resource/App/Weave/Book.php
@@ -7,15 +7,10 @@
 namespace FakeVendor\Sandbox\Resource\App\Weave;
 
 use BEAR\Resource\Annotation\FakeLog;
-use BEAR\Resource\ResourceInterface;
 use BEAR\Resource\ResourceObject;
 
 class Book extends ResourceObject
 {
-    public function __construct(ResourceInterface $resource = null)
-    {
-    }
-
     /**
      * @FakeLog
      */
