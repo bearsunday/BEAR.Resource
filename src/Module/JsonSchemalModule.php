93805aed62751cf7a92a01c80c8f2c0eb857709e
YAMANE Nana <shigematsu.nana@gmail.com>
2018-07-15 18:20:38 +0900

    revert JsonSchemalModule(mark deprecated)

---
+++ src/Module/JsonSchemalModule.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the BEAR.Resource package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\Resource\Module;
+
+use BEAR\Resource\Annotation\JsonSchema;
+use BEAR\Resource\Interceptor\JsonSchemaInterceptor;
+use BEAR\Resource\ResourceObject;
+use Ray\Di\AbstractModule;
+
+/**
+ * @deprecated
+ */
+class JsonSchemalModule extends AbstractModule
+{
+    /**
+     * Json-schema json file directory
+     *
+     * @var string
+     */
+    private $jsonSchemaDir;
+
+    /**
+     * Json-schema validator json file directory
+     *
+     * @var string
+     */
+    private $jsonValidateDir;
+
+    public function __construct($jsonSchemaDir = '', $jsonValidateDir = '', AbstractModule $module = null)
+    {
+        $this->jsonSchemaDir = $jsonSchemaDir;
+        $this->jsonValidateDir = $jsonValidateDir;
+        parent::__construct($module);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->bind()->annotatedWith('json_schema_dir')->toInstance($this->jsonSchemaDir);
+        $this->bind()->annotatedWith('json_validate_dir')->toInstance($this->jsonValidateDir);
+        $this->bindInterceptor(
+            $this->matcher->subclassesOf(ResourceObject::class),
+            $this->matcher->annotatedWith(JsonSchema::class),
+            [JsonSchemaInterceptor::class]
+        );
+    }
+}
