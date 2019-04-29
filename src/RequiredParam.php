bdb3dc38cef4a849cff1ded30c77207450a483e8
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-04-29 20:52:16 +0900

    try CamelCase name when request parameter matching failed
    camelCase parameters in ResourceObject method can be called snake_case query parameter.
    
    `?usser_id=1` can call`onGet($userId)` method.

--- src/RequiredParam.php
+++ src/RequiredParam.php
@@ -14,10 +14,15 @@ final class RequiredParam implements ParamInterface
      */
     public function __invoke(string $varName, array $query, InjectorInterface $injector)
     {
-        unset($injector);
         if (isset($query[$varName])) {
             return $query[$varName];
         }
+        // try camelCase variable name
+        $snakeName = ltrim(strtolower(preg_replace('/[A-Z]/', '_\0', $varName)), '_');
+        if (isset($query[$snakeName])) {
+            return $query[$snakeName];
+        }
+        unset($injector);
 
         throw new ParameterException($varName);
     }
