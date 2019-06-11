45be4f16a8fc1dbce13102f84b7399376a788106
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-06-11 09:50:34 +0900

    change log level according to the method
    GET = DEBUG
    UNSAFE = INFO

--- src/DevPsrLogger.php
+++ src/DevPsrLogger.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace BEAR\Resource;
 
 use Psr\Log\LoggerInterface as PsrLoggerInterface;
+use Psr\Log\LogLevel;
 use function sprintf;
 
 final class DevPsrLogger implements LoggerInterface
@@ -22,7 +23,8 @@ final class DevPsrLogger implements LoggerInterface
     public function __invoke(ResourceObject $ro) : void
     {
         $msg = sprintf('request: %s %s', $ro->uri->method, (string) $ro->uri);
-        $this->logger->debug($msg);
-        $this->logger->debug(sprintf('response %s', $ro->code), json_decode((string) clone $ro, true));
+        $level = ($ro->uri->method === 'get') ? LogLevel::DEBUG : LogLevel::INFO;
+        $this->logger->log($level, $msg);
+        $this->logger->log($level, sprintf('response %s', $ro->code), json_decode((string) clone $ro, true));
     }
 }
