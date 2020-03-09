1e392059abe5cc80c12a555050591dab406f9824
Yoshitaka Jingu <jingu@bitnauts.co.jp>
2020-03-09 19:24:35 +0900

    avoid link if resource code is NOT_FOUND

---
+++ tests/Fake/FakeVendor/Sandbox/Resource/App/Blog/NotFound.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the BEAR.Resource package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace FakeVendor\Sandbox\Resource\App\Blog;
+
+use BEAR\Resource\Annotation\Link;
+use BEAR\Resource\Code;
+use BEAR\Resource\ResourceObject;
+
+class NotFound extends ResourceObject
+{
+    /**
+     * @return $this
+     * @Link(href="app://self/user{?id}", rel="user", crawl="meta")
+     */
+    public function onGet()
+    {
+        $this->body = ['message' => 'blog not found'];
+        $this->code = Code::NOT_FOUND;
+
+        return $this;
+    }
+}
