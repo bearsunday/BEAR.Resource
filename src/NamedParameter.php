60d2b5851c02a05d581f5f435bd52f696a3b1744
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-05-26 01:10:41 +0900

    fix MixedArgumentTypeCoercion

--- src/NamedParameter.php
+++ src/NamedParameter.php
@@ -29,10 +29,11 @@ final class NamedParameter implements NamedParameterInterface
      */
     public function getParameters(callable $callable, array $query) : array
     {
+        /** @var array<string, ParamInterface> $metas */
         $metas = ($this->paramMetas)($callable);
         $parameters = [];
+        /** @var array<string, array<string, mixed>> $query */
         foreach ($metas as $varName => $param) {
-            /* @var $param ParamInterface */
             /** @psalm-suppress MixedAssignment */
             $parameters[] = $param($varName, $query, $this->injector);
         }
