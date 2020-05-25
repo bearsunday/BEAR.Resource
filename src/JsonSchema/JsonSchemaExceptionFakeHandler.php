463dda29ae7f21ebfcda9b4b426cc412bcf221c8
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-05-25 18:05:41 +0900

    soothe psalm

--- src/JsonSchema/JsonSchemaExceptionFakeHandler.php
+++ src/JsonSchema/JsonSchemaExceptionFakeHandler.php
@@ -25,23 +25,25 @@ class JsonSchemaExceptionFakeHandler implements JsonSchemaExceptionHandlerInterf
     }
 
     /**
-     * @return array<string, mixed>
+     * @return array<int|string, mixed>
      */
     private function fakeResponse(string $schemaFile) : array
     {
+        /** @var array<int|string, mixed> $fakeObject */
         $fakeObject = (new Faker)->generate(new \SplFileInfo($schemaFile));
 
         return $this->deepArray($fakeObject);
     }
 
     /**
-     * @param array<string, mixed> $values
+     * @param array<int|string, mixed> $values
      *
-     * @return array<string, mixed>
+     * @return array<int|string, mixed>
      */
     private function deepArray($values) : array
     {
         $result = [];
+        /** @psalm-suppress MixedAssignment */
         foreach ($values as $key => $value) {
             $result[$key] = is_object($value) ? $this->deepArray((array) $value) : $result[$key] = $value;
         }
