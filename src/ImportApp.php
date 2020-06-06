395c6147788acd8a1b6fed266e464f9d5cae7df8
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-06-06 11:13:10 +0900

    add param type and remove phpdoc

--- src/ImportApp.php
+++ src/ImportApp.php
@@ -7,32 +7,21 @@ namespace BEAR\Resource;
 final class ImportApp
 {
     /**
-     * Host name
-     *
      * @var string
      */
     public $host;
 
     /**
-     * App name
-     *
      * @var string
      */
     public $appName;
 
     /**
-     * Context
-     *
      * @var string
      */
     public $context;
 
-    /**
-     * @param string $host
-     * @param string $appName
-     * @param string $context
-     */
-    public function __construct($host, $appName, $context)
+    public function __construct(string $host, string $appName, string $context)
     {
         $this->host = $host;
         $this->appName = $appName;
