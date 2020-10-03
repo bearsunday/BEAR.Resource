520fd438c7976adc4c9ad0b123e8a9d08f532082
koriym <akihito.koriyama@gmail.com>
2020-10-04 02:26:57 +0900

    Update HalLinkTest.php

--- tests/HalLinkTest.php
+++ tests/HalLinkTest.php
@@ -10,14 +10,14 @@ use PHPUnit\Framework\TestCase;
 class HalLinkTest extends TestCase
 {
     /**
-     * @covers \BEAR\Resource\HalLink::linkAnnotation()
+     * @covers \BEAR\Resource\HalLink::bodyLink()
      */
     public function testBodyLinkInvalidLink(): void
     {
         $halLink = new HalLink(new NullReverseLink());
         $body = [
             '_links' => [
-                ['rel' => 'not-href'],
+                'rel1' => 'not-href',
             ],
         ];
         $hal = $halLink->addHalLink($body, [], new Hal());
