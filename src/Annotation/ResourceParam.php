9094368bc96b530e0ed6e1373b908eed0af7da95
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-01-10 15:21:42 +0900

    Chane annotation compatible attribute

--- src/Annotation/ResourceParam.php
+++ src/Annotation/ResourceParam.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace BEAR\Resource\Annotation;
 
+use Attribute;
+use function putenv;
+
 /**
  * @Annotation
  * @Target("METHOD")
  */
-final class ResourceParam
+#[Attribute]
+final class ResourceParam implements RequestParamInterface
 {
     /** @var string */
     public $param;
@@ -17,5 +21,14 @@ final class ResourceParam
     public $uri;
 
     /** @var bool */
-    public $templated = false;
+    public $templated;
+
+    /** @param array{uri?: string, param?: string, templated?: bool} $values */
+    public function __construct(array $values = [], string $uri = '', string $param = '', bool $templated = false)
+    {
+        $this->uri = $values['uri'] ?? $uri;
+        $this->param = $values['param'] ?? $param;
+        $this->templated = $values['templated'] ?? $templated;
+    }
 }
+
