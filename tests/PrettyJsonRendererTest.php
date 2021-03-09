0a2e24596bf0cc24a9954f655f36ed91a7a86563
koriym <akihito.koriyama@gmail.com>
2021-03-09 11:34:18 +0900

    Add red test
    Embedded resource in embedded  resource is not rendered.

---
+++ tests/PrettyJsonRendererTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BEAR\Resource;
+
+use BEAR\Resource\Module\EmbedResourceModule;
+use BEAR\Resource\Module\ResourceModule;
+use PHPUnit\Framework\TestCase;
+use Ray\Di\Injector;
+
+class PrettyJsonRendererTest extends TestCase
+{
+    /** @var PrettyJsonRenderer */
+    private $renderer;
+
+    public function setUp(): void
+    {
+        $this->renderer = new PrettyJsonRenderer();
+    }
+
+    public function testRender(): void
+    {
+        $ro = new NullResourceObject();
+        $ro->body = ['a' => ['b' => 'c']];
+        $this->assertSame('{
+    "a": {
+        "b": "c"
+    }
+}
+', $this->renderer->render($ro));
+    }
+
+    public function testRenderWithEmbeded(): void
+    {
+        $resource = (new Injector(new EmbedResourceModule(new ResourceModule('FakeVendor\Sandbox')), __DIR__ . '/tmp'))->getInstance(ResourceInterface::class);
+        $ro = $resource->get('app://self/bird/embed-birds', ['id' => '1']);
+        $this->assertSame('{
+    "birds": {
+        "bird1": {
+            "name": "chill kun"
+        },
+        "bird2": {
+            "sparrow_id": "1"
+        }
+    }
+}
+', $this->renderer->render($ro));
+    }
+}
