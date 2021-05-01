7caeb33023369b3725b291512529ffdbae427aea
koriym <akihito.koriyama@gmail.com>
2021-05-01 09:32:40 +0900

    Update ResourceModule.php

--- src/Module/ResourceModule.php
+++ src/Module/ResourceModule.php
@@ -30,10 +30,6 @@ class ResourceModule extends AbstractModule
         $this->install(new AnnotationModule());
         $this->install(new EmbedResourceModule());
         $this->install(new HttpClientModule());
-        if (! $this->appName) {
-            return;
-        }
-
         $this->bind()->annotatedWith(AppName::class)->toInstance($this->appName);
     }
 }
