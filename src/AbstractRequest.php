d6e954d6f83098cb5e67084315d0a85acee3d2a9
koriym <akihito.koriyama@gmail.com>
2021-11-06 01:22:59 +0900

    Add @codeCoverageIgnore

--- src/AbstractRequest.php
+++ src/AbstractRequest.php
@@ -274,6 +274,8 @@ abstract class AbstractRequest implements RequestInterface, ArrayAccess, Iterato
 
     /**
      * @param array<mixed> $data
+     *
+     * @codeCoverageIgnore
      */
     public function __unserialize(array $data): void
     {
