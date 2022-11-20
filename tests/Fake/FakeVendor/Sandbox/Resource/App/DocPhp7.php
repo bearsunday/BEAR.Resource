4a7b0a1af2fe37a98430b48bd2de922b56f5ef33
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-11-20 11:22:43 +0900

    Change attribute to annotation for the test coverage

--- tests/Fake/FakeVendor/Sandbox/Resource/App/DocPhp7.php
+++ tests/Fake/FakeVendor/Sandbox/Resource/App/DocPhp7.php
@@ -11,14 +11,19 @@ use Ray\WebContextParam\Annotation\ServerParam;
 
 class DocPhp7 extends ResourceObject
 {
+    // Annotations (@ResourceParam, @Assisted) are intentionally used for testing.
+
     /**
      * @param int    $id          Id
      * @param string $name        Name
      * @param bool   $sw          Swithc
      * @param string $login_id    Login ID
      * @param string $defaultNull DefaultNull
+     *
+     * @ResourceParam(uri="app://self/login#id")
+     * @Assisted({"titme"})
      */
-    public function onGet(int $id, string $name, bool $sw, #[ResourceParam(uri: "app://self/login#id")] string $login_id, array $arr, #[Assisted] string $time, $defaultNull = null)
+    public function onGet(int $id, string $name, bool $sw, string $login_id, array $arr, string $time, $defaultNull = null)
     {
         return $this;
     }
