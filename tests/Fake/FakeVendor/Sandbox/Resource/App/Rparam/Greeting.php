7f1da83d0393ca1564b57a7b2275e02017345ca0
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-11-20 11:10:57 +0900

    Test coverage for the program with annotation

--- tests/Fake/FakeVendor/Sandbox/Resource/App/Rparam/Greeting.php
+++ tests/Fake/FakeVendor/Sandbox/Resource/App/Rparam/Greeting.php
@@ -6,10 +6,19 @@ namespace FakeVendor\Sandbox\Resource\App\Rparam;
 
 use BEAR\Resource\Annotation\ResourceParam;
 use BEAR\Resource\ResourceObject;
+use Ray\Di\Di\Assisted;
 
 class Greeting extends ResourceObject
 {
-    public function onGet(#[ResourceParam(uri: "app://self/rparam/login#login_id")] string $name = null)
+    /**
+     * ResourceParam annotated class
+     *
+     * This is not an intentional attribute to test annotations.
+     *
+     * @ResourceParam(uri="app://self/rparam/login#login_id", param="name")
+     * @Assisted({"assisted"})
+     */
+    public function onGet(string $name = null, string $assisted = null)
     {
         $this['name'] = $name;
 
@@ -20,7 +29,10 @@ class Greeting extends ResourceObject
     {
     }
 
-    public function onPost(#[ResourceParam(uri: "app://self/rparam/login{?name}#nickname", templated: true)] string $id, string $name)
+    /**
+     * @ResourceParam(uri="app://self/rparam/login{?name}#nickname", templated=true, param="id")
+     */
+    public function onPost(string $id, string $name)
     {
         $this['id'] = $id;
 
