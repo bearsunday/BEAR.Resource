6e25cea52064326b5ead4aa33128678fea78a09a
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-04-29 10:52:58 +0900

    Soothe psalm: Refactor HttpRequestCurl to improve type safety
    This commit focuses on refactoring the HttpRequestCurl class to enhance its type safety. Various changes have been made including removing unnecessary functions, adding type annotations, splitting a complex function into smaller, more manageable ones, and changing the way headers are processed. This will help improve code readability, maintenance and reduce potential typing errors.

--- src/HttpRequestCurl.php
+++ src/HttpRequestCurl.php
@@ -6,8 +6,7 @@ namespace BEAR\Resource;
 
 use CurlHandle;
 
-use function array_keys;
-use function array_map;
+use function assert;
 use function count;
 use function curl_close;
 use function curl_exec;
@@ -16,7 +15,6 @@ use function curl_init;
 use function curl_setopt;
 use function explode;
 use function http_build_query;
-use function is_array;
 use function json_decode;
 use function json_encode;
 use function parse_str;
@@ -35,29 +33,27 @@ use const CURLOPT_POSTFIELDS;
 use const CURLOPT_RETURNTRANSFER;
 use const CURLOPT_URL;
 
+// ... Other code is omitted for brevity
+
+/**
+ * Sends a HTTP request using cURL
+ *
+ * @psalm-type RequestOptions = array<null>|array{"body?": string, "headers?": array<string, string>}
+ * @psalm-type Headers = array<string, string>
+ * @psalm-type Body = array<mixed>
+ */
 final class HttpRequestCurl
 {
     /**
-     * Set the properties of a ResourceObject.
-     *
-     * @param ResourceObject              $ro      The ResourceObject to set the properties for.
-     * @param string                      $method  The HTTP method to use for the request.
-     * @param string                      $uri     The URL to send the request to.
-     * @param array<string, string|array> $options Additional options for the request (optional).
-     *
-     * @return void
-     */
-
-    /**
      * Sends a HTTP request using cURL
      *
-     * @param string                                                      $method  The HTTP method (GET, POST, PUT, DELETE, etc.).
-     * @param string                                                      $uri     The URL of the request.
-     * @param array<null>|array{"body": string, "headers": array<string>} $options Additional options for the request.
+     * @param string         $method  The HTTP method (GET, POST, PUT, DELETE, etc.).
+     * @param string         $uri     The URL of the request.
+     * @param RequestOptions $options Additional options for the request.
      *                       - headers: An array of headers to be sent with the request.
      *                       - body: The request body.
      *
-     * @return array{body: array<array-key, mixed>, code: int, headers: array<string, string>, view: string}
+     * @return array{body: Body, code: int, headers: Headers, view: string}
      *      An associative array containing the response information.
      *     - code: The HTTP response code.
      *     - headers: An array of response headers.
@@ -66,43 +62,81 @@ final class HttpRequestCurl
      */
     public function request(string $method, string $uri, array $options = []): array
     {
-        $curl = curl_init();
-        // Set Request Method
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-        // Set Request URL
-        curl_setopt($curl, CURLOPT_URL, $uri);
-        $requestHeaders = [];
-        if (isset($options['headers']) && is_array($options['headers'])) {
-            foreach ($options['headers'] as $header) {
-                $parts = explode(':', $header, 2);
-                if (count($parts) !== 2) {
-                    continue;
-                }
+        $curl = $this->initializeCurl($method, $uri, $options);
+        assert($curl instanceof CurlHandle);
+        $response = (string) curl_exec($curl);
+        $code = (int) curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $headerSize = (int) curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+        $headerString = substr($response, 0, $headerSize);
+        $view = substr($response, $headerSize);
+        /** @var array<string, string> $headers */
+        $headers = $this->parseResponseHeaders($headerString);
+        curl_close($curl);
 
-                $requestHeaders[$parts[0]] = trim($parts[1]);
+        $body = $this->parseBody($curl, $view);
+
+        return [
+            'code' => $code,
+            'headers' => $headers,
+            'body' => $body,
+            'view' => $view,
+        ];
+    }
+
+    /** @param RequestOptions $options $ */
+    private function initializeCurl(string $method, string $uri, array $options): CurlHandle|bool
+    {
+        $curl = curl_init();
+        assert($curl instanceof CurlHandle);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($curl, CURLOPT_URL, $uri);
+
+        $requestHeaders = [];
+        if (isset($options['headers'])) {
+            $optionsHeaders = $options['headers'];
+            /** @psalm-trace  $optionsHeaders */
+            $requestHeaders = $this->headersFromOptions($optionsHeaders);
+            if (! empty($requestHeaders)) {
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $requestHeaders);
             }
         }
 
-        if (! empty($requestHeaders)) {
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array_map(static function ($key, $value) {
-                return "$key: $value";
-            }, array_keys($requestHeaders), $requestHeaders));
-        }
-
         if (isset($options['body'])) {
-            /** @var string $optionBody */
             $optionBody = $options['body'];
-            $contentType = $requestHeaders['Content-Type'] ?? 'application/x-www-form-urlencoded';
+            $contentType = $requestHeaders['content-type'] ?? $requestHeaders['Content-Type'] ?? 'application/x-www-form-urlencoded';
             $this->setCurlRequestPayload($contentType, $curl, $optionBody);
         }
 
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_HEADER, true);
-        $response = (string) curl_exec($curl);
-        $responseCode = (int) curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        $headerSize = (int) curl_getinfo($curl, CURLINFO_HEADER_SIZE);
-        $responseHeaders = substr($response, 0, $headerSize);
-        $view = substr($response, $headerSize);
+
+        return $curl;
+    }
+
+    /**
+     * Extracts headers from the options array
+     *
+     * @param Headers $headers
+     *
+     * @return array<string, string>
+     */
+    private function headersFromOptions(array $headers): array
+    {
+        $requestHeaders = [];
+        foreach ($headers as $header) {
+            $parts = explode(':', $header, 2);
+            if (count($parts) !== 2) {
+                continue;
+            }
+
+            $requestHeaders[$parts[0]] = trim($parts[1]);
+        }
+
+        return $requestHeaders;
+    }
+
+    private function parseResponseHeaders(string $responseHeaders): array
+    {
         $responseHeadersArray = [];
         $headerLines = explode("\r\n", $responseHeaders);
         foreach ($headerLines as $line) {
@@ -111,29 +145,28 @@ final class HttpRequestCurl
                 continue;
             }
 
-            $responseHeadersArray[$parts[0]] = trim($parts[1]);
+            $key = $parts[0];
+
+            $responseHeadersArray[$key] = trim($parts[1]);
         }
 
+        return $responseHeadersArray;
+    }
+
+    private function parseBody(CurlHandle $curl, string $view): array
+    {
         $responseBody = [];
         $contentType = (string) curl_getinfo($curl, CURLINFO_CONTENT_TYPE);
         if (strpos(strtolower($contentType), 'application/json') !== false) {
-            /** @var array<string, mixed> $responseBody */
-            $responseBody = json_decode($view, true);
+            $responseBody = (array) json_decode($view, true);
         } elseif (strpos(strtolower($contentType), 'application/x-www-form-urlencoded') !== false) {
             parse_str($view, $responseBody);
         }
 
-        curl_close($curl);
-
-        return [
-            'code' => $responseCode,
-            'headers' => $responseHeadersArray,
-            'body' => $responseBody,
-            'view' => $view,
-        ];
+        return $responseBody;
     }
 
-    private function setCurlRequestPayload(mixed $contentType, CurlHandle|bool $curl, string $optionBody): void
+    private function setCurlRequestPayload(string $contentType, CurlHandle $curl, string|array $optionBody): void
     {
         $strippedContentType = strtolower($contentType);
         $isJson = strpos($strippedContentType, 'application/json') !== false;
