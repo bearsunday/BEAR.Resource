477f8917bc5d9a8f815c993f3fa409b23856bb46
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-06-27 04:59:44 +0900

    Refactor InputParamMeta to utilize OptionsMethodRequest for enhanced parameter processing and organization

--- src/Options/InputParamMeta.php
+++ src/Options/InputParamMeta.php
@@ -8,13 +8,11 @@ use BEAR\Resource\InputAttributeIterator;
 use ReflectionClass;
 use ReflectionMethod;
 use ReflectionNamedType;
+use ReflectionParameter;
 
 use function array_merge;
 use function class_exists;
-use function is_array;
-use function is_bool;
-use function is_numeric;
-use function is_string;
+use function in_array;
 
 /**
  * Extracts metadata from Input attribute parameters
@@ -23,11 +21,13 @@ final class InputParamMeta implements InputParamMetaInterface
 {
     private InputAttributeIterator $inputIterator;
     private OptionsMethodDocBolck $docBlock;
+    private OptionsMethodRequest $methodRequest;
 
     public function __construct()
     {
         $this->inputIterator = new InputAttributeIterator();
         $this->docBlock = new OptionsMethodDocBolck();
+        $this->methodRequest = new OptionsMethodRequest();
     }
 
     /**
@@ -97,84 +97,65 @@ final class InputParamMeta implements InputParamMetaInterface
             return [[], []];
         }
 
-        // Get constructor documentation using OptionsMethodDocBolck
-        // This already handles type conversion (int -> integer) and description extraction
+        // Use OptionsMethodRequest to handle parameter processing
         [, $constructorParamDocs] = ($this->docBlock)($constructor);
+        $paramMeta = ($this->methodRequest)($constructor, $constructorParamDocs, []);
 
-        $parameters = [];
-        $required = [];
+        $parameters = $paramMeta['parameters'] ?? [];
+        $required = $paramMeta['required'] ?? [];
+
+        // Separate regular parameters and nested Input parameters
+        $finalParameters = [];
+        $finalRequired = [];
 
         foreach ($constructor->getParameters() as $param) {
             $paramName = $param->getName();
 
-            // Check if this is a nested Input parameter
-            if ($this->inputIterator->getInputAttribute($param) !== null) {
-                $type = $param->getType();
-                if ($type instanceof ReflectionNamedType && class_exists($type->getName())) {
-                    $nestedDescription = null;
-                    if (isset($constructorParamDocs[$paramName]['description'])) {
-                        $nestedDescription = $constructorParamDocs[$paramName]['description'];
-                    }
+            if ($this->isNestedInputParameter($param)) {
+                $nestedDescription = $constructorParamDocs[$paramName]['description'] ?? null;
+                [$nestedParams, $nestedRequired] = $this->processNestedParameter(
+                    $param,
+                    $nestedDescription,
+                );
+                $finalParameters = array_merge($finalParameters, $nestedParams);
+                $finalRequired = array_merge($finalRequired, $nestedRequired);
+            } elseif (isset($parameters[$paramName])) {
+                // Add group information to regular parameters
+                $parameters[$paramName]['group'] = $group;
+                if ($groupDescription) {
+                    $parameters[$paramName]['group_description'] = $groupDescription;
+                }
 
-                    [$nestedParams, $nestedRequired] = $this->flattenInputClass(
-                        $type->getName(),
-                        $paramName,
-                        $nestedDescription,
-                    );
+                $finalParameters[$paramName] = $parameters[$paramName];
 
-                    $parameters = array_merge($parameters, $nestedParams);
-                    $required = array_merge($required, $nestedRequired);
-                    continue;
+                if (in_array($paramName, $required, true)) {
+                    $finalRequired[] = $paramName;
                 }
             }
-
-            // Start with phpdoc information (already has type conversion)
-            $paramMeta = [];
-            if (isset($constructorParamDocs[$paramName])) {
-                $paramMeta = $constructorParamDocs[$paramName];
-            }
-
-            // Override with reflection type if phpdoc doesn't have type
-            if (! isset($paramMeta['type'])) {
-                $type = $param->getType();
-                if ($type instanceof ReflectionNamedType) {
-                    $typeName = $type->getName();
-                    // Apply same conversion as OptionsMethodDocBolck
-                    $paramMeta['type'] = $typeName === 'int' ? 'integer' : $typeName;
-                }
-            }
-
-            // Add default value
-            if ($param->isDefaultValueAvailable()) {
-                /** @var mixed $defaultValue */
-                $defaultValue = $param->getDefaultValue();
-                if (is_string($defaultValue)) {
-                    $paramMeta['default'] = $defaultValue;
-                } elseif (is_bool($defaultValue)) {
-                    $paramMeta['default'] = $defaultValue ? 'true' : 'false';
-                } elseif (is_numeric($defaultValue)) {
-                    $paramMeta['default'] = (string) $defaultValue;
-                } elseif (is_array($defaultValue)) {
-                    $paramMeta['default'] = '[]';
-                }
-            }
-
-            // Add group information
-            $paramMeta['group'] = $group;
-            if ($groupDescription) {
-                $paramMeta['group_description'] = $groupDescription;
-            }
-
-            $parameters[$paramName] = $paramMeta;
-
-            // Check if required
-            if ($param->isDefaultValueAvailable() || $param->allowsNull()) {
-                continue;
-            }
-
-            $required[] = $paramName;
         }
 
-        return [$parameters, $required];
+        return [$finalParameters, $finalRequired];
+    }
+
+    private function isNestedInputParameter(ReflectionParameter $param): bool
+    {
+        return $this->inputIterator->getInputAttribute($param) !== null;
+    }
+
+    /** @return array{0: array<string, array<string, mixed>>, 1: array<int, string>} */
+    private function processNestedParameter(
+        ReflectionParameter $param,
+        string|null $nestedDescription,
+    ): array {
+        $type = $param->getType();
+        if (! ($type instanceof ReflectionNamedType) || ! class_exists($type->getName())) {
+            return [[], []];
+        }
+
+        return $this->flattenInputClass(
+            $type->getName(),
+            $param->getName(),
+            $nestedDescription,
+        );
     }
 }
